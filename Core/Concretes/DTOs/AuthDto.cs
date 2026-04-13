e16ab68dbe536c212b49a1ca57c6ebf2a5f1eb9b
ahmetcanayhan <ahmetayhan38@icloud.com>
2026-04-13 15:00:22 +0300

    Update 02 13.04.2026

---
+++ Core/Concretes/DTOs/AuthDto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Concretes.DTOs
+{
+    public class LoginDto
+    {
+        [Required, Display(Name ="Email Address", Prompt ="Email Address"), EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required, Display(Name = "Password", Prompt = "Password"), DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name ="Remember Me", Prompt ="Remember Me")]
+        public bool RememberMe { get; set; }
+
+    }
+
+    public class RegisterDto
+    {
+        [Required, Display(Name = "Email Address", Prompt = "Email Address"), EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required, Display(Prompt ="FirstName")]
+        public string FirstName { get; set; } = null!;
+
+        [Required, Display(Prompt ="LastName")]
+        public string LastName { get; set; } = null!;
+
+        [Required, Display(Name = "Password", Prompt = "Password"), DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required, Display(Name = "ConfirmPassword", Prompt = "ConfirmPassword"), DataType(DataType.Password), Compare("Password")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+
+    public class ChangePasswordDto
+    {
+        [Required, Display(Name = "PreviousPassword", Prompt = "PreviousPassword"), DataType(DataType.Password)]
+        public string PreviousPassword { get; set; } = null!;
+
+        [Required, Display(Name = "NewPassword", Prompt = "NewPassword"), DataType(DataType.Password)]
+        public string NewPassword { get; set; } = null!;
+
+        [Required, Display(Name = "Password", Prompt = "Password"), DataType(DataType.Password), Compare("ConfirmPassword")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+
+    public class ResetPasswordDto
+    {
+        [Required, Display(Name = "NewPassword", Prompt = "NewPassword"), DataType(DataType.Password)]
+        public string NewPassword { get; set; } = null!;
+
+        [Required, Display(Name = "Password", Prompt = "Password"), DataType(DataType.Password), Compare("ConfirmPassword")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [Required]
+        public string AuthToken { get; set; } = null!;
+    }
+}
